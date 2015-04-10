f6f977acd76a46686760a51c95ef0f00eccf54ab
Eric <naisanza@gmail.com>
2015-04-10 19:16:40 -0400

    Update eric.sh

--- eric.sh
+++ eric.sh
@@ -5,17 +5,27 @@
 
 
 function _main() {
-    _compatibleVersion=$1
+    _cV=$1	# Compatible Version
 
-    _name[1]=shell.lib
+    _name[1]=ariel.lib
     _hash[1]=3495faa48796ee2bc37eba138ff12c7a
     unset _name[0]
     unset _hash[0]
 
-    n=$(echo ${#_hash[@]})
-    echo "Newest Version: $n"
-    echo "Name: ${_name[$n]}"
-    echo "Hash: ${_hash[$n]}"
+    _nV=$(echo ${#_hash[@]})	# Newest Version
+    echo "Newest Version: $_nV"
+    echo "Name: ${_name[$_nV]}"
+    echo "Hash: ${_hash[$_nV]}"
+    echo ""
+    if [ ! $_cV > ${#_hash[@]} ]; then
+	echo "Compatible Version: $_cV"
+	echo "Name: ${_name[$_cV]}"
+	echo "Hash: ${_hash[$_cV]}"
+    else
+	echo "The compatible version is newer than what Eric knows"
+	echo "Please find a better Eric. Good bye"
+	exit 1
+    fi
 
     cd $(dirname $0)
 
@@ -24,33 +34,33 @@ function _main() {
     while true; do
 	echo "Searching $(pwd)..."
 
-	find $(pwd) -type f -iname "*${_name[$n]}" | grep "${_name[$n]}" > $_tmp	
-	if [ ! $? == 0 ]; then
+	find $(pwd) -type f -iname "*${_name[$_cV]}" | grep "${_name[$_cV]}" > $_tmp	
+	if [ ! $? == 0 ]; then		# Loop
 	    if [ ! $(pwd) == "/" ]; then
-		cd ..
+	  	cd ..
 	    else
-
-# Add in loop for searching other names
-		
-		echo "Not found"
+		echo "Ariel cannot be found"
+		echo "You have no library on this system. Good bye"
 		exit 1
 	    fi
 	else
-	    echo "Found! Locations:"
+	    echo "Found!"
 	    cat $_tmp
 	    echo "Verifing..."
 	    
 	    while read line; do
-		_hashV=$(md5sum $line | grep -oE "^[a-zA-Z0-9]*")
+		_hashV=$(md5sum "$line" | grep -oE "^[a-zA-Z0-9]*")	# Hash of found versions
 		
-		for (( nV=0; nV<${#hash[@]}; nV++ )); do
-		    if [ $_hashV == ${_hash[$nV]} ]; then
-# Finish This
-			echo ""
-		    fi
-		done
-
+		if [ $_hashV == ${_hash[$_cV]} ]; then
+		    echo "Compatible: $line"
+		    export _ariel=$line
+		    return 0
+		else
+		    echo "Not Compatible: $line"
+		    cd ..
+		fi
 	    done < $_tmp
+	    
 	fi
 }
 
