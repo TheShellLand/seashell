3d9535218e8ed8de9f69b7d1d37fec35b80bf12f
Eric <naisanza@gmail.com>
2015-04-10 20:03:48 -0400

    Update eric.sh

--- eric.sh
+++ eric.sh
@@ -18,15 +18,18 @@ function _main() {
     echo "Hash: ${_hash[$_nV]}"
     echo ""
     if [ ! -z "$_cV" ]; then
-	if [ ! "$_cV" > ${#_hash[@]} ]; then
+	if [ ! "$_cV" -gt ${#_hash[@]} ]; then
 	    echo "Compatible Version: $_cV"
 	    echo "Name: ${_name[$_cV]}"
 	    echo "Hash: ${_hash[$_cV]}"
+	    echo ""
 	else
 	    echo "The compatible version is newer than what Eric knows"
 	    echo "Please find a better Eric. Good bye"
 	    exit 1
 	fi
+    else
+	_cV=$_nV
     fi
 
     cd $(dirname $0)
@@ -36,25 +39,21 @@ function _main() {
     while true; do
 	echo "Searching $(pwd)..."
 
-	find $(pwd) -type f -iname "*${_name[$_cV]}" | grep "${_name[$_cV]}" > $_tmp	
+	find $(pwd) -type f -iname "*${_name[$_cV]}" 2>/dev/null | grep "${_name[$_cV]}" > $_tmp	
 	if [ ! $? == 0 ]; then		# Loop
 	    if [ ! $(pwd) == "/" ]; then
 	  	cd ..
-	    else
-		echo "Ariel cannot be found"
-		echo "You have no library on this system. Good bye"
-		exit 1
 	    fi
 	else
-	    echo "Found!"
-	    cat $_tmp
-	    echo "Verifing..."
-	    
+
 	    while read line; do
+		echo "Found!"
+		echo "Verifing..."
+
 		_hashV=$(md5sum "$line" | grep -oE "^[a-zA-Z0-9]*")	# Hash of found versions
 		
-		if [ $_hashV == ${_hash[$_cV]} ]; then
-		    echo "Compatible: $line"
+		if [ "$_hashV" == "${_hash[$_cV]}" ]; then
+		    echo "Compatible: $_hashV $line"
 		    export _ariel=$line
 		    return 0
 		else
@@ -62,8 +61,15 @@ function _main() {
 		    cd ..
 		fi
 	    done < $_tmp
-	    
+
+	    if [ $(pwd) == "/" ]; then
+		echo "Ariel cannot be found"
+		echo "You have no library on this system. Good bye"
+		exit 1
+	    fi
+
 	fi
+	
     done
 }
 
